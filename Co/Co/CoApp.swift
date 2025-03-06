e6206b1101cbff606723b2b810f36e7635a75c6f
Above <134832174+eqycc@users.noreply.github.com>
2025-03-06 02:59:50 -0800

    i
    SwiftUI

---
+++ Co/Co/CoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoApp.swift
+//  Co
+//
+//  Created by A on 06/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
